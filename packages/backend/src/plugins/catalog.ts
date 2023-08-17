7731ead93e62574219a94bdaf2a6def5525870ae
Frank Kong <frkong@redhat.com>
2023-08-17 17:29:13 -0400

    chore: add ansible automation platform (AAP)(1.1.0) plugin

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,11 +2,25 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { AapResourceEntityProvider } from '@janus-idp/backstage-plugin-aap-backend';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
+  builder.addEntityProvider(
+    AapResourceEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 1 },
+        timeout: { minutes: 1 },
+      }),
+    }),
+  );
+
+
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
